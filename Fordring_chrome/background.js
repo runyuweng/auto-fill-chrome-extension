397ef6ba98899e911574accd33fc0a290cdc4d50
wengwengweng <593110501@qq.com>
2017-03-27 22:28:11 +0800

    update

--- Fordring_chrome/background.js
+++ Fordring_chrome/background.js
@@ -7,6 +7,11 @@ chrome.extension.onRequest.addListener(
       })
       sendResponse({message:'success'});
     }else if(request.type === 'fill'){
+      console.log('background get fill');
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {type: request.type, text: localStorage}, function(response) {
+        });
+      });
       sendResponse({message:'success'});
     }
 
--- Fordring_chrome/myscript.js
+++ Fordring_chrome/myscript.js
@@ -1,9 +1,16 @@
-let translate = true;
-
 // 监听popup点击事件
+
+const resume={
+  fill:function(resume){
+      //使用jquery填写简历内容
+  }
+}
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-    console.log(request);
-    
+    console.log('myscript get request',request);
+    if(request.type === 'fill'){
+      resume.fill(request.text);
+    }
   }
 );
